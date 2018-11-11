f72bca6a05e915af19f755c4d3772eb6686bb559
mostafaAli122 <mostafaali122@outlook.com>
2018-11-11 10:47:34 +0200

    Verified account

--- profile.php
+++ profile.php
@@ -2,15 +2,20 @@
 include('./classes/DB.php');
 include('./classes/Login.php');
 $username = "";
+$verified = False;
 $isFollowing = False;
 if (isset($_GET['username'])) {
         if (DB::query('SELECT username FROM users WHERE username=:username', array(':username'=>$_GET['username']))) {
                 $username = DB::query('SELECT username FROM users WHERE username=:username', array(':username'=>$_GET['username']))[0]['username'];
                 $userid = DB::query('SELECT id FROM users WHERE username=:username', array(':username'=>$_GET['username']))[0]['id'];
+                $verified = DB::query('SELECT verified FROM users WHERE username=:username', array(':username'=>$_GET['username']))[0]['verified'];
                 $followerid = Login::isLoggedIn();
                 if (isset($_POST['follow'])) {
                         if ($userid != $followerid) {
                                 if (!DB::query('SELECT follower_id FROM followers WHERE user_id=:userid', array(':userid'=>$userid))) {
+                                        if ($followerid == 6) {
+                                                DB::query('UPDATE users SET verified=1 WHERE id=:userid', array(':userid'=>$userid));
+                                        }
                                         DB::query('INSERT INTO followers VALUES (\'\', :userid, :followerid)', array(':userid'=>$userid, ':followerid'=>$followerid));
                                 } else {
                                         echo 'Already following!';
@@ -21,6 +26,9 @@ if (isset($_GET['username'])) {
                 if (isset($_POST['unfollow'])) {
                         if ($userid != $followerid) {
                                 if (DB::query('SELECT follower_id FROM followers WHERE user_id=:userid', array(':userid'=>$userid))) {
+                                        if ($followerid == 6) {
+                                                DB::query('UPDATE users SET verified=0 WHERE id=:userid', array(':userid'=>$userid));
+                                        }
                                         DB::query('DELETE FROM followers WHERE user_id=:userid AND follower_id=:followerid', array(':userid'=>$userid, ':followerid'=>$followerid));
                                 }
                                 $isFollowing = False;
@@ -35,7 +43,7 @@ if (isset($_GET['username'])) {
         }
 }
 ?>
-<h1><?php echo $username; ?>'s Profile</h1>
+<h1><?php echo $username; ?>'s Profile<?php if ($verified) { echo ' - Verified'; } ?></h1>
 <form action="profile.php?username=<?php echo $username; ?>" method="post">
         <?php
         if ($userid != $followerid) {
