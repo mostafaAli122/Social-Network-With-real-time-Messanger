79bd7fe3bd5a18571c5871b467e0a8ee5675856a
mostafaAli122 <mostafaali122@outlook.com>
2018-11-12 11:49:26 +0200

    upload coments on posts on timeline

--- classes/comment.php
+++ classes/comment.php
@@ -10,5 +10,11 @@ class Comment {
                         DB::query('INSERT INTO comments VALUES (\'\', :comment, :userid, NOW(), :postid)', array(':comment'=>$commentBody, ':userid'=>$userId, ':postid'=>$postId));
                 }
         }
+        public static function displayComments($postId) {
+                $comments = DB::query('SELECT comments.comment, users.username FROM comments, users WHERE post_id = :postid AND comments.user_id = users.id', array(':postid'=>$postId));
+                foreach($comments as $comment) {
+                        echo $comment['comment']." ~ ".$comment['username']."<hr />";
+                }
+        }
 }
 ?>
