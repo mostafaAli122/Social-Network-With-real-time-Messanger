3a92250ce0c45512b11eddaa7cce1ec2b0f41dc6
mostafaAli122 <mostafaali122@outlook.com>
2018-11-12 21:16:20 +0200

    Activate Hashing# And get all related Posts To Specific Hashtag

--- classes/post.php
+++ classes/post.php
@@ -4,8 +4,9 @@ class Post {
                 if (strlen($postbody) > 160 || strlen($postbody) < 1) {
                         die('Incorrect length!');
                 }
+                $topics = self::getTopics($postbody);
                 if ($loggedInUserId == $profileUserId) {
-                        DB::query('INSERT INTO posts VALUES (\'\', :postbody, NOW(), :userid, 0, \'\')', array(':postbody'=>$postbody, ':userid'=>$profileUserId));
+                        DB::query('INSERT INTO posts VALUES (\'\', :postbody, NOW(), :userid, 0, \'\', :topics)', array(':postbody'=>$postbody, ':userid'=>$profileUserId, ':topics'=>$topics));
                 } else {
                         die('Incorrect user!');
                 }
@@ -14,8 +15,9 @@ class Post {
                 if (strlen($postbody) > 160) {
                         die('Incorrect length!');
                 }
+                $topics = self::getTopics($postbody);
                 if ($loggedInUserId == $profileUserId) {
-                        DB::query('INSERT INTO posts VALUES (\'\', :postbody, NOW(), :userid, 0, \'\')', array(':postbody'=>$postbody, ':userid'=>$profileUserId));
+                        DB::query('INSERT INTO posts VALUES (\'\', :postbody, NOW(), :userid, 0, \'\', \'\')', array(':postbody'=>$postbody, ':userid'=>$profileUserId, ':topics'=>$topics));
                         $postid = DB::query('SELECT id FROM posts WHERE user_id=:userid ORDER BY ID DESC LIMIT 1;', array(':userid'=>$loggedInUserId))[0]['id'];
                         return $postid;
                 } else {
@@ -31,75 +33,15 @@ class Post {
                         DB::query('DELETE FROM post_likes WHERE post_id=:postid AND user_id=:userid', array(':postid'=>$postId, ':userid'=>$likerId));
                 }
         }
-        public static function link_add($text) {
+        public static function getTopics($text) {
                 $text = explode(" ", $text);
-                $newstring = "";
+                $topics = "";
                 foreach ($text as $word) {
-                        if (substr($word, 0, 1) == "@") {
-                                $newstring .= "<a href='profile.php?username=".substr($word, 1)."'>".htmlspecialchars($word)."</a> ";
-                        } else {
-                                $newstring .= htmlspecialchars($word)." ";
+                        if (substr($word, 0, 1) == "#") {
+                                $topics .= substr($word, 1).",";
                         }
                 }
-                return $newstring;
-        }
-        public static function displayPosts($userid, $username, $loggedInUserId) {
-                $dbposts = DB::query('SELECT * FROM posts WHERE user_id=:userid ORDER BY id DESC', array(':userid'=>$userid));
-                $posts = "";
-                foreach($dbposts as $p) {
-                        if (!DB::query('SELECT post_id FROM post_likes WHERE post_id=:postid AND user_id=:userid', array(':postid'=>$p['id'], ':userid'=>$loggedInUserId))) {
-                                $posts .= "<img src='".$p['postimg']."'>".self::link_add($p['body'])."
-                                <form action='profile.php?username=$username&postid=".$p['id']."' method='post'>
-                                        <input type='submit' name='like' value='Like'>
-                                        <span>".$p['likes']." likes</span>
-                                </form>
-                                <hr /></br />
-                                ";
-                        } else {
-                                $posts .= "<img src='".$p['postimg']."'>".self::link_add($p['body'])."
-                                <form action='profile.php?username=$username&postid=".$p['id']."' method='post'>
-                                        <input type='submit' name='unlike' value='Unlike'>
-                                        <span>".$p['likes']." likes</span>
-                                </form>
-                                <hr /></br />
-                                ";
-                        }
-                }
-                return $posts;
-        }
-}
-?><?php
-class Post {
-        public static function createPost($postbody, $loggedInUserId, $profileUserId) {
-                if (strlen($postbody) > 160 || strlen($postbody) < 1) {
-                        die('Incorrect length!');
-                }
-                if ($loggedInUserId == $profileUserId) {
-                        DB::query('INSERT INTO posts VALUES (\'\', :postbody, NOW(), :userid, 0, \'\')', array(':postbody'=>$postbody, ':userid'=>$profileUserId));
-                } else {
-                        die('Incorrect user!');
-                }
-        }
-        public static function createImgPost($postbody, $loggedInUserId, $profileUserId) {
-                if (strlen($postbody) > 160) {
-                        die('Incorrect length!');
-                }
-                if ($loggedInUserId == $profileUserId) {
-                        DB::query('INSERT INTO posts VALUES (\'\', :postbody, NOW(), :userid, 0, \'\')', array(':postbody'=>$postbody, ':userid'=>$profileUserId));
-                        $postid = DB::query('SELECT id FROM posts WHERE user_id=:userid ORDER BY ID DESC LIMIT 1;', array(':userid'=>$loggedInUserId))[0]['id'];
-                        return $postid;
-                } else {
-                        die('Incorrect user!');
-                }
-        }
-        public static function likePost($postId, $likerId) {
-                if (!DB::query('SELECT user_id FROM post_likes WHERE post_id=:postid AND user_id=:userid', array(':postid'=>$postId, ':userid'=>$likerId))) {
-                        DB::query('UPDATE posts SET likes=likes+1 WHERE id=:postid', array(':postid'=>$postId));
-                        DB::query('INSERT INTO post_likes VALUES (\'\', :postid, :userid)', array(':postid'=>$postId, ':userid'=>$likerId));
-                } else {
-                        DB::query('UPDATE posts SET likes=likes-1 WHERE id=:postid', array(':postid'=>$postId));
-                        DB::query('DELETE FROM post_likes WHERE post_id=:postid AND user_id=:userid', array(':postid'=>$postId, ':userid'=>$likerId));
-                }
+                return $topics;
         }
         public static function link_add($text) {
                 $text = explode(" ", $text);
@@ -107,6 +49,8 @@ class Post {
                 foreach ($text as $word) {
                         if (substr($word, 0, 1) == "@") {
                                 $newstring .= "<a href='profile.php?username=".substr($word, 1)."'>".htmlspecialchars($word)."</a> ";
+                        } else if (substr($word, 0, 1) == "#") {
+                                $newstring .= "<a href='topics.php?topic=".substr($word, 1)."'>".htmlspecialchars($word)."</a> ";
                         } else {
                                 $newstring .= htmlspecialchars($word)." ";
                         }
