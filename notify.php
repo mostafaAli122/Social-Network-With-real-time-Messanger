a48a34f12f57ea3b86cbbe0d8718029851ff00c2
mostafaAli122 <mostafaali122@outlook.com>
2018-11-13 20:09:22 +0200

    Liking  Notification

--- notify.php
+++ notify.php
@@ -8,7 +8,7 @@ if (Login::isLoggedIn()) {
 }
 echo "<h1>Notifcations</h1>";
 if (DB::query('SELECT * FROM notifications WHERE receiver=:userid', array(':userid'=>$userid))) {
-        $notifications = DB::query('SELECT * FROM notifications WHERE receiver=:userid', array(':userid'=>$userid));
+        $notifications = DB::query('SELECT * FROM notifications WHERE receiver=:userid ORDER BY id DESC', array(':userid'=>$userid));
         foreach($notifications as $n) {
                 if ($n['type'] == 1) {
                         $senderName = DB::query('SELECT username FROM users WHERE id=:senderid', array(':senderid'=>$n['sender']))[0]['username'];
@@ -18,6 +18,9 @@ if (DB::query('SELECT * FROM notifications WHERE receiver=:userid', array(':user
                                 $extra = json_decode($n['extra']);
                                 echo $senderName." mentioned you in a post! - ".$extra->postbody."<hr />";
                         }
+                } else if ($n['type'] == 2) {
+                        $senderName = DB::query('SELECT username FROM users WHERE id=:senderid', array(':senderid'=>$n['sender']))[0]['username'];
+                        echo $senderName." liked your post!<hr />";
                 }
         }
 }
