b67445c70982e1a452d970cbabaa229372119d6d
mostafaAli122 <mostafaali122@outlook.com>
2018-11-14 17:33:59 +0200

    Designing The Create-account Page And Using using API to login users using jQuery and AJAX to enable us to access our API without reloading the page

--- api/index.php
+++ api/index.php
@@ -1,25 +1,56 @@
 <?php
 require_once("DB.php");
-
 $db = new DB("127.0.0.1", "SocialNetwork", "root", "");
-
 if ($_SERVER['REQUEST_METHOD'] == "GET") {
-
         if ($_GET['url'] == "auth") {
-
         } else if ($_GET['url'] == "users") {
-
         }
-
 } else if ($_SERVER['REQUEST_METHOD'] == "POST") {
-
+        if ($_GET['url'] == "users") {
+                $postBody = file_get_contents("php://input");
+                $postBody = json_decode($postBody);
+                $username = $postBody->username;
+                $email = $postBody->email;
+                $password = $postBody->password;
+                if (!$db->query('SELECT username FROM users WHERE username=:username', array(':username'=>$username))) {
+                        if (strlen($username) >= 3 && strlen($username) <= 32) {
+                                if (preg_match('/[a-zA-Z0-9_]+/', $username)) {
+                                        if (strlen($password) >= 6 && strlen($password) <= 60) {
+                                        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                                        if (!$db->query('SELECT email FROM users WHERE email=:email', array(':email'=>$email))) {
+                                                $db->query('INSERT INTO users VALUES (\'\', :username, :password, :email, \'0\', \'\')', array(':username'=>$username, ':password'=>password_hash($password, PASSWORD_BCRYPT), ':email'=>$email));
+                                                echo '{ "Success": "User Created!" }';
+                                                http_response_code(200);
+                                        } else {
+                                                echo '{ "Error": "Email in use!" }';
+                                                http_response_code(409);
+                                        }
+                                } else {
+                                        echo '{ "Error": "Invalid Email!" }';
+                                        http_response_code(409);
+                                        }
+                                } else {
+                                        echo '{ "Error": "Invalid Password!" }';
+                                        http_response_code(409);
+                                }
+                                } else {
+                                        echo '{ "Error": "Invalid Username!" }';
+                                        http_response_code(409);
+                                }
+                        } else {
+                                echo '{ "Error": "Invalid Username!" }';
+                                http_response_code(409);
+                        }
+                } else {
+                        echo '{ "Error": "User exists!" }';
+                        http_response_code(409);
+                }
+        }
         if ($_GET['url'] == "auth") {
                 $postBody = file_get_contents("php://input");
                 $postBody = json_decode($postBody);
-
                 $username = $postBody->username;
                 $password = $postBody->password;
-
                 if ($db->query('SELECT username FROM users WHERE username=:username', array(':username'=>$username))) {
                         if (password_verify($password, $db->query('SELECT password FROM users WHERE username=:username', array(':username'=>$username))[0]['password'])) {
                                 $cstrong = True;
@@ -35,9 +66,7 @@ if ($_SERVER['REQUEST_METHOD'] == "GET") {
                         echo '{ "Error": "Invalid username or password!" }';
                         http_response_code(401);
                 }
-
         }
-
 }  else if ($_SERVER['REQUEST_METHOD'] == "DELETE") {
         if ($_GET['url'] == "auth") {
                 if (isset($_GET['token'])) {
